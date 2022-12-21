9e030da4f2d411f9e1b1422c5b071e98c95a5c60
felixDev22 <felixnzomo@gmail.com>
2022-12-21 08:29:48 +0300

    Add variables

---
+++ src/modules/variables.js
@@ -0,0 +1,7 @@
+const inputScore = document.getElementById('board');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+const submit = document.getElementById('submit');
+
+// eslint-disable-next-line object-curly-newline
+export { inputScore, name, score, submit };
