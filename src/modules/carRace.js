94e66fb4e190acc16aa7b7c6f0d6be7b731b66b8
felixDev22 <felixnzomo@gmail.com>
2022-12-22 22:01:30 +0300

    Add refresh id, update carRace.js & Display.js

--- src/modules/carRace.js
+++ src/modules/carRace.js
@@ -1,9 +1,26 @@
-class carRace {
-  constructor(game) {
-    this.game = game;
-    this.apiUrl =
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-  }
-}
+// get the data from API
 
-saveGame = async () => {};
+const getRaceScore = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/KfbBpq9lwKHbgKVV1Z1q/scores'
+  );
+  const results = await response.json();
+  const scoreList = results.result;
+  return scoreList;
+};
+
+const addScore = async (user) => {
+  const data = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/KfbBpq9lwKHbgKVV1Z1q/scores',
+    {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+    }
+  );
+  return data;
+};
+
+export { getRaceScore, addScore };
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,38 +1,14 @@
 import { inputScore, name, score } from './variables.js';
+import getRaceScore from './getApiScore.js';
 
 let scoreArr = [];
 
-// Save To Local Directory
-const pushToLocal = () => {
-  localStorage.setItem('inputScore', JSON.stringify(scoreArr));
-};
-
-// To get recent scores and push to LocalStorage
+// To get recent scores
 const addScore = () => {
   const user = {};
   user.name = name.value;
   user.score = score.value;
   scoreArr.push(user);
-  pushToLocal();
+  name.value = '';
+  score.value = '';
 };
-
-// To render dynamically the recent scores
-const generateScore = () => {
-  scoreArr.forEach((user) => {
-    inputScore.innerHTML += `
-        <li>${user.name}: ${user.score}</li>`;
-    name.value = '';
-    score.value = '';
-  });
-};
-
-// To display recent added scores
-const showScore = () => {
-  if (localStorage.getItem('inputScore')) {
-    scoreArr = JSON.parse(localStorage.getItem('inputScore'));
-  }
-
-  generateScore();
-};
-
-export { generateScore, addScore, showScore };
