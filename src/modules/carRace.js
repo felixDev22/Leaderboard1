d65dbcf5fda3d9ab155ee50425ed9697d6f25deb
felixDev22 <felixnzomo@gmail.com>
2022-12-22 20:29:34 +0300

    Add carRace.js,update READme

---
+++ src/modules/carRace.js
@@ -0,0 +1,9 @@
+class carRace {
+  constructor(game) {
+    this.game = game;
+    this.apiUrl =
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+  }
+}
+
+saveGame = async () => {};
